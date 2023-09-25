1a628000aa1e7c557c76448585cdfcf2751b000f
AliPatrick71 <53902582+AliPatrick71@users.noreply.github.com>
2023-09-25 15:07:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1> BallotOnline</h1>
+        <p></p>
+        <?php
+            echo "<p>Hello from BallotOnline ...</p>\n";
+            echo "<p>Sample BallotOnline System.</p>\n";
+            echo "<p>Our Online voting system coming soon for clients around the world </p>\n";
+            echo "date : " . date('j-m-y h-i-s');
+            echo "<p>System developed using PHP by Idriss Bakayoko!!!</p>\n";
+        ?>
+    </body>
+
+</html>
